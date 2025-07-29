d73838347142b510d74f89594fe2f0313b7391a4
tao-shen <57491236+tao-shen@users.noreply.github.com>
2025-07-29 17:06:21 +0000

    Deploying to gh-pages from @ tao-shen/tao-shen.github.io@868919a0c4280261f2b254018b78d12cd07bf577 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,13 +433,6 @@ ninja.data = [{
           window.open("https://github.com/tao-shen", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-orcid',
         title: 'ORCID',
         section: 'Socials',
